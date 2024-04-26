aacb907be54cfc15fa823f43ff4601173a437aa4
Jude Wilson <hi@judes.club>
2024-04-25 20:12:25 -0400

    first examples

--- Sources/VerbuiseCore/VerbuiseCore.swift
+++ Sources/VerbuiseCore/VerbuiseCore.swift
@@ -1,2 +1,28 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+
+private struct VerbuiseKeys {
+    var apiKey: String = ""
+    var project: String = ""
+    var organization: String = ""
+}
+
+private var InitiatedProject = VerbuiseKeys()
+
+func VerbuiseInit(apiKey: String, username: String, projectName: String) -> Bool {
+    InitiatedProject.apiKey = apiKey
+    InitiatedProject.project = projectName
+    InitiatedProject.organization = username
+    
+    // value validation
+    
+    return true
+}
+
+extension String {
+    func localize() -> String {
+        return self.replacingOccurrences(of: " ", with: "_")
+    }
+}
